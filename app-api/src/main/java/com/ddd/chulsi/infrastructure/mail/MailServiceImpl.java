be43260bd86f7130cc940feae9627a35e3f87e3c
오태현 <jeffrey@kai-i.com>
2023-07-14 21:05:04 +0900

    이메일 본문 Naver 기준 css 안먹힘
    - 전부 style 로 수정
    - 기본값으로 443 도메인이 아니면 이미지 나오지 않는 중 (테스트 완료)

--- app-api/src/main/java/com/ddd/chulsi/infrastructure/mail/MailServiceImpl.java
+++ app-api/src/main/java/com/ddd/chulsi/infrastructure/mail/MailServiceImpl.java
@@ -38,78 +38,40 @@ public class MailServiceImpl implements MailService {
                 // HTML 적용됨
                 String content = """
                     <!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                    	<meta charset="UTF-8">
-                    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    	<title>Document</title>
-                    	<style>
-                    		.mc {
-                                  color: #00CC8F;
-                              }
-                              .grey {
-                                  color: #808080;
-                              }
-                              .fs16 {
-                                  font-size: 16px;
-                              }
-                              .reset-btn {
-                                  width: 200px;
-                                  height: 50px;
-                                  border-radius: 4px;
-                                  background-color: #00CC8F;
-                                  border-color: #FFF;
-                                  border-width: 0px;
-                                  font-size: 16px;
-                                  color: #FFF;
-                              }
-                              .mt4 {
-                                  margin-top: 4px;
-                              }
-                              .mt20 {
-                                  margin-top: 20px;
-                              }
-                              .mt40 {
-                                  margin-top: 40px;
-                              }
-                              .container {
-                                  width: 640px;
-                                  height: 460px;
-                              }
-                              .header {
-                                  width: 120px;
-                              }
-                              .header > img {
-                                  width: 100%;
-                              }
-                    	</style>
-                    </head>
-                    <body>
-                    	<div class="container">
-                           <div class="header">
-                               <img src="{images-directory}/sikdorok_logo.png" alt="sikdorok">
-                               <div class="mc fs16 mt4"><b>비밀번호 재설정</b></div>
+                   <html lang="ko">
+                   
+                   <head>
+                       <meta charset="UTF-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                       <title>식도록</title>
+                   </head>
+                   
+                   <body>
+                       <div style="width: 640px; height: 460px;">
+                           <div style="width: 120px;">
+                               <img src="{images-directory}/sikdorok_logo.png" alt="sikdorok" style="width: 100%;">
+                               <div style="color: #00CC8F; font-size: 16px; margin-top: 4px;"><b>비밀번호 재설정</b></div>
                            </div>
-                           <div class="mt40">
-                               <b>{nickname}</b>님 안녕하세요.<br/>
+                           <div style="margin-top: 40px;">
+                               <b>{nickname}</b>님 안녕하세요.<br />
                                아래 버튼을 선택하여 비밀번호를 재설정해주세요.
                            </div>
-                           <div class="mt20">
-                               <button class="reset-btn" type="button">비밀번호 재설정하기</button>
+                           <div style="margin-top: 20px;">
+                               <button style="width: 200px; height: 50px; border-radius: 4px; background-color: #00CC8F; border-color: #FFF; border-width: 0px; font-size: 16px; color: #FFF;" type="button">비밀번호 재설정하기</button> </div>
+                           <div style="margin-top: 20px;">
+                               <span style="color: #808080;">(비밀번호 재설정 링크는 1시간 동안 유효합니다.)</span>
                            </div>
-                           <div class="mt20">
-                               <span class="grey">(비밀번호 재설정 링크는 1시간 동안 유효합니다.)</span>
-                           </div>
-                           <div class="mt40">
+                           <div style="margin-top: 40px;">
                                <img src="{images-directory}/strap.png" alt="strap">
                            </div>
-                           <div class="mt20">
-                               <span class="grey">문의사항은 team.sikdorok@gmail.com으로 연락 주시기 바랍니다.</span><br />
-                               <span class="grey">&copy; sikdorok All rights reserved.</span>
+                           <div style="margin-top: 20px;">
+                               <span style="color: #808080;">문의사항은 team.sikdorok@gmail.com으로 연락 주시기 바랍니다.</span><br />
+                               <span style="color: #808080;">&copy; sikdorok All rights reserved.</span>
                            </div>
                        </div>
-                    </body>
-                    </html>
+                   </body>
+                   
+                   </html>
                     """;
                 content = content.replace("{images-directory}", imagesDirectory);
                 content = content.replace("{nickname}", users.getName());
