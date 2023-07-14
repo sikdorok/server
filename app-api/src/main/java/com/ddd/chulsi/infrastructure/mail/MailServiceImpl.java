33a65f0a2b6ff72d23f34cc76171d187d621320b
오태현 <jeffrey@kai-i.com>
2023-07-14 20:22:46 +0900

    이메일에 사용할 이미지 3배수로 변경 및 1차 작업 완료

--- app-api/src/main/java/com/ddd/chulsi/infrastructure/mail/MailServiceImpl.java
+++ app-api/src/main/java/com/ddd/chulsi/infrastructure/mail/MailServiceImpl.java
@@ -45,80 +45,72 @@ public class MailServiceImpl implements MailService {
                     	<title>Document</title>
                     	<style>
                     		.mc {
-                    			color: #00CC8F;
-                    		}
-                    		.grey {
-                    			color: #808080;
-                    		}
-                    		.fs14 {
-                    			font-size: 14px;
-                    		}
-                    		.reset-btn {
-                    			width: 200px;
-                    			height: 50px;
-                    			border-radius: 4px;
-                    			background-color: #00CC8F;
-                    			border-color: #FFF;
-                    			border-width: 0px;
-                    			font-size: 16px;
-                    			color: #FFF;
-                    		}
-                    		.mt4 {
-                    			margin-top: 4px;
-                    		}
-                    		.mt14 {
-                    			margin-top: 14px;
-                    		}
-                    		.mt20 {
-                    			margin-top: 20px;
-                    		}
-                    		.mt40 {
-                    			margin-top: 40px;
-                    		}
-                    		.container {
-                    			width: 640px;
-                    			height: 360px;
-                    		}
-                    		.header {
-                    			width: 120px;
-                    		}
-                    		.header > img {
-                    			width: 100%;
-                    		}
-                    		.taco > img {
-                    			position: fixed;
-                    			left: 480px;
-                    			top: 180px;
-                    		}
+                                  color: #00CC8F;
+                              }
+                              .grey {
+                                  color: #808080;
+                              }
+                              .fs16 {
+                                  font-size: 16px;
+                              }
+                              .reset-btn {
+                                  width: 200px;
+                                  height: 50px;
+                                  border-radius: 4px;
+                                  background-color: #00CC8F;
+                                  border-color: #FFF;
+                                  border-width: 0px;
+                                  font-size: 16px;
+                                  color: #FFF;
+                              }
+                              .mt4 {
+                                  margin-top: 4px;
+                              }
+                              .mt20 {
+                                  margin-top: 20px;
+                              }
+                              .mt40 {
+                                  margin-top: 40px;
+                              }
+                              .container {
+                                  width: 640px;
+                                  height: 460px;
+                              }
+                              .header {
+                                  width: 120px;
+                              }
+                              .header > img {
+                                  width: 100%;
+                              }
                     	</style>
                     </head>
                     <body>
                     	<div class="container">
-                    		<div class="header">
-                    			<img src="{images-directory}/sikdorok_logo.png" alt="sikdorok">
-                    			<div class="mc fs14 mt4">비밀번호 재설정</div>
-                    		</div>
-                    		<div class="mt20">
-                    			<b>{nickname}</b>님 안녕하세요.<br/>
-                    			아래 버튼을 선택하여 비밀번호를 재설정해주세요.
-                    		</div>
-                    		<div class="mt14">
-                    			<button class="reset-btn" type="button">비밀번호 재설정하기</button>
-                    		</div>
-                    		<div class="mt14">
-                    			<span class="grey">(비밀번호 재설정 링크는 1시간 동안 유효합니다.)</span>
-                    		</div>
-                    		<div class="taco">
-                    			<img src="{images-directory}/taco.png" alt="taco">
-                    		</div>
-                    		<div class="mt40">
-                    			<img src="{images-directory}/strap.png" alt="strap">
-                    		</div>
-                    		<div class="mt14">
-                    			<span class="grey">문의사항은 team.sikdorok@gmail.com으로 연락 주시기 바랍니다.</span><br />
-                    			<span class="grey">&copy; sikdorok All rights reserved.</span>
-                    		</div>
-                    	</div>
+                           <div class="header">
+                               <img src="{images-directory}/sikdorok_logo.png" alt="sikdorok">
+                               <div class="mc fs16 mt4"><b>비밀번호 재설정</b></div>
+                           </div>
+                           <div class="mt40">
+                               <b>{nickname}</b>님 안녕하세요.<br/>
+                               아래 버튼을 선택하여 비밀번호를 재설정해주세요.
+                           </div>
+                           <div class="mt20">
+                               <button class="reset-btn" type="button">비밀번호 재설정하기</button>
+                           </div>
+                           <div class="mt20">
+                               <span class="grey">(비밀번호 재설정 링크는 1시간 동안 유효합니다.)</span>
+                           </div>
+                           <div class="taco" style="position: fixed; right: 240px; top: 160px;">
+                               <img src="{images-directory}/taco.png" alt="taco">
+                           </div>
+                           <div class="mt40">
+                               <img src="{images-directory}/strap.png" alt="strap">
+                           </div>
+                           <div class="mt20">
+                               <span class="grey">문의사항은 team.sikdorok@gmail.com으로 연락 주시기 바랍니다.</span><br />
+                               <span class="grey">&copy; sikdorok All rights reserved.</span>
+                           </div>
+                       </div>
                     </body>
                     </html>
                     """;
