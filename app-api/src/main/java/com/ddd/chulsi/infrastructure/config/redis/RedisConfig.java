239cc0559265bba4ebb0931e63c61f67ce23ed31
오태현 <jeffrey@kai-i.com>
2023-08-10 20:22:12 +0900

    Redis 연결 실패 대응

--- app-api/src/main/java/com/ddd/chulsi/infrastructure/config/redis/RedisConfig.java
+++ app-api/src/main/java/com/ddd/chulsi/infrastructure/config/redis/RedisConfig.java
@@ -1,6 +1,9 @@
 package com.ddd.chulsi.infrastructure.config.redis;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.TimeoutOptions;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,16 +11,16 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.time.Duration;
 
-@RequiredArgsConstructor
-@Configuration
-@EnableRedisRepositories
+@Slf4j
 @EnableAsync
+@Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
@@ -28,9 +31,15 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
+        ClientOptions clientOptions = ClientOptions.builder()
+            .autoReconnect(true)
+            .timeoutOptions(TimeoutOptions.enabled())
+            .disconnectedBehavior(ClientOptions.DisconnectedBehavior.DEFAULT)
+            .build();
+
         LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
             .commandTimeout(Duration.ofMinutes(1))
-            .shutdownTimeout(Duration.ZERO)
+            .clientOptions(clientOptions)
             .build();
 
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
@@ -42,7 +51,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
     }
