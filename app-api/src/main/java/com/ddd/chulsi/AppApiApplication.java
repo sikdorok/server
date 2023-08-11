d2097c99b6282e71098efd9c5de97231a7d50822
오태현 <jeffrey@kai-i.com>
2023-08-11 18:50:09 +0900

    Redis Repository Scan 관련 exclude 처리 및 HikariCP `max-lifetime` 수정

--- app-api/src/main/java/com/ddd/chulsi/AppApiApplication.java
+++ app-api/src/main/java/com/ddd/chulsi/AppApiApplication.java
@@ -2,12 +2,11 @@ package com.ddd.chulsi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
 @EnableJpaAuditing // AuditingEntityListener 를 사용하기 위함
 @ConfigurationPropertiesScan
 public class AppApiApplication {
