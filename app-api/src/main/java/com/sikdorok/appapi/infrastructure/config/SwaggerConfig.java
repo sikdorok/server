d9020d7b1d52d6091cca559d5f9343c4e42e30b0
오태현 <jeffrey@kai-i.com>
2024-03-11 13:42:42 +0900

    Fix: SwaggerConfig 수정
    - Servers Url 추가

--- app-api/src/main/java/com/sikdorok/appapi/infrastructure/config/SwaggerConfig.java
+++ app-api/src/main/java/com/sikdorok/appapi/infrastructure/config/SwaggerConfig.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -25,6 +27,7 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
 
         return new OpenAPI()
+            .servers(List.of(localhost(), develop()))
             .components(new Components().addSecuritySchemes("JWT", securityScheme))
             .security(Collections.singletonList(securityRequirement))
             .info(
@@ -33,6 +36,18 @@ public class SwaggerConfig {
                     .description("식도록 API")
                     .version("v1.0.0")
             );
+    }
+
+    private Server localhost() {
+        return new Server()
+            .url("http://localhost:8080")
+            .description("Local Server");
+    }
+
+    private Server develop() {
+        return new Server()
+            .url("https://sikdorok.jeffrey-oh.click")
+            .description("Develop Server");
     }
 
 }
